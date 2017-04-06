a42b63dc314ff5288c0d32b1399170162dff1bc6
Maxim O <public.iam@gmail.com>
2017-04-06 20:09:13 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+M/Monit Mattermost notification script
+======================================
+
+A script for [M/Monit](https://mmonit.com/) to enable notifications to a [Mattermost](http://www.mattermost.org/) server.
+
+## Script Usage
+
+Run `./mm-notify.py --help` for full usage information.
+
+## Mattermost Configuration
+
+Both M/Monit and Monit can quite easy be setup to send alerts to almost any online service. This is especially straightforward if the service sports a HTTP API. In this example we demonstrate how this can be done by sending notification to Mattermost.
+
+On the Mattermost side you will first need to activate an integration and obtain a token integrations by adding an "Incoming WebHooks" integration. 
+
+Solution is most idential as Slak integration: https://mmonit.com/wiki/MMonit/SlackNotification
+
+```
+/usr/local/bin/mm-notify.py --url https://mattermost.server/hooks/<token> --notificationtype "$MONIT_ACTION"
+```
---
+++ mm-notify.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python2
+
+# Copyright (c) 2017 Maxim Odinintsev
+# 
+# Based on Nagios notification plugin by NDrive
+# https://github.com/NDrive/nagios-mattermost
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import argparse
+import json
+import urllib2
+import os
+
+VERSION = "0.0.1"
+
+def parse():
+    parser = argparse.ArgumentParser(description='Sends alerts to Mattermost')
+    parser.add_argument('--url', help='Incoming Webhook URL', required=True)
+    parser.add_argument('--channel', help='Channel to notify')
+    parser.add_argument('--username', help='Username to notify as',
+                        default='m/monit notify')
+    parser.add_argument('--iconurl', help='URL of icon to use for username',
+                        default='https://mmonit.com/monit/img/logo.png') # noqa
+    parser.add_argument('--notificationtype', help='Notification Type', default='none')
+    parser.add_argument('--version', action='version',
+                        version='% (prog)s {version}'.format(version=VERSION))
+    args = parser.parse_args()
+    return args
+
+
+def encode_special_characters(text):
+    text = text.replace("%", "%25")
+    return text
+
+
+def make_data(args):
+    template = "**" + os.environ['MONIT_ACTION'] + "**\n**" + os.environ['MONIT_EVENT'] + "**\n\nHost affected: **" + os.environ['MONIT_HOST'] + "**\nService affected: **" + os.environ['MONIT_SERVICE'] + "**\nDetails: " + os.environ['MONIT_DESCRIPTION']
+
+    # Emojis
+    print args.notificationtype.lower()
+    if args.notificationtype.lower() == "alert":
+        EMOJI = ":sos: "
+    elif args.notificationtype.lower() == "stop":
+        EMOJI = ":red_circle: "
+    elif args.notificationtype.lower() == "start":
+        EMOJI = ":white_check_mark: "
+    elif args.notificationtype.lower() == "restart":
+        EMOJI = ":arrows_counterclockwise: "
+    elif args.notificationtype.lower() == "exec":
+  EMOJI = ":interrobang: "
+    elif args.notificationtype.lower() == "unmonitor":
+  EMOJI = ":heavy_exclamation_mark: "
+    else:
+        EMOJI = ""
+
+    text = EMOJI + template.format(**vars(args))
+
+    payload = {
+        "username": args.username,
+        "icon_url": args.iconurl,
+        "text": encode_special_characters(text)
+    }
+
+    if args.channel:
+        payload["channel"] = args.channel
+
+    data = "payload=" + json.dumps(payload)
+    return data
+
+
+def request(url, data):
+    req = urllib2.Request(url, data)
+    response = urllib2.urlopen(req)
+    return response.read()
+
+
+if __name__ == "__main__":
+    args = parse()
+    data = make_data(args)
+    response = request(args.url, data)
+    print response
